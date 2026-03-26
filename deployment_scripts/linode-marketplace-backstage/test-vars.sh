9007db3481372d4a482f201cd16dd1c0fb5c6c74
ssastrau <ssastrau@akamai.com>
2026-03-26 10:12:53 +0100

    test

--- deployment_scripts/linode-marketplace-backstage/test-vars.sh
+++ deployment_scripts/linode-marketplace-backstage/test-vars.sh
@@ -2,6 +2,8 @@
 
 DEFAULT_DNS="$(hostname -I | awk '{print $1}'| tr '.' '-' | awk {'print $1 ".ip.linodeusercontent.com"'})"
 
+RANDOM_SUBDOMAIN="a$(tr -dc 'a-z0-9' </dev/urandom | head -c7)"
+
 # custom env variables from cli
 if [[ -n ${INSTANCE_ENV} ]]; then
   custom_vars=(${INSTANCE_ENV})
@@ -40,7 +42,7 @@ fi
 if [[ -n "${SUBDOMAIN}" ]]; then
         UDF_VARS["SUBDOMAIN"]="${SUBDOMAIN}"
 else
-        UDF_VARS["SUBDOMAIN"]="www" # default
+        UDF_VARS["SUBDOMAIN"]="${RANDOM_SUBDOMAIN}"
 fi
 
 if [[ -n "${DOMAIN}" ]]; then
